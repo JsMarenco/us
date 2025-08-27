3d5c0b892b039b8ac7f0d1d3da57c8cc48e1bd7f
Angel Marenco <jsmarenco.dev@gmail.com>
2025-08-27 15:51:31 -0600

    adapter changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import vercelStatic from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aniversario.cocoton.net/",
+  site: "https://cocoton.net/",
   integrations: [mdx(), sitemap(), react()],
-  output: "static",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: vercelStatic({
-    webAnalytics: {
-      enabled: false,
-    },
-    maxDuration: 8,
-  }),
 });
